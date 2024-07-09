c901b25033e71236c5ae4490d34391aee82f253e
Kyle Grealis <kylegrealis@icloud.com>
2024-07-09 17:09:07 -0500

    initial commit: create data dir, load & write data to CSV

---
+++ data-import.R
@@ -0,0 +1,18 @@
+# This script is to install the necessary R packages, create a data directory, and read & write the NASCAR Cup Series driver career data to a CSV file. The file that is written will be loaded in Python to render the plot for the plotnine contest.
+
+if (!require('readr')) install.packages('readr')
+
+# setup to access data from nascaR.data package
+if (!require('remotes')) install.packages('remotes')
+
+# install nascaR.data package
+remotes::install_github('kyleGrealis/nascaR.data')
+
+# create data folder
+if (!dir.exists(paste0(here::here(), "/data"))) {
+  dir.create(paste0(here::here(), "/data"))
+}
+
+# load & save data
+driver_data <- nascaR.data::cup_driver_career
+readr::write_csv(driver_data, 'data/driver_data.csv')
