fc2fc8780d4b044dab758da7b6b3002b8730d024
Kyle Grealis <kylegrealis@icloud.com>
2024-07-12 13:21:48 -0500

    edit: adding annotations and subtitle

--- plot.py
+++ plot.py
@@ -1,61 +1,95 @@
 # %%
+import pandas as pd
 from plotnine import *
-import polars as pl
+from siuba import (_, arrange, case_when, count, filter, group_by, inner_join,
+    mutate, ungroup, summarize)
+
+
+import_df = (
+    pd.read_csv('data/cup_race_data.csv', low_memory=False)
+    >> filter(_.season == 2024)
+)
+
+counts = (
+    import_df
+    >> group_by(_.driver)
+    >> count()
+    >> filter(_.n >= 10)
+    >> ungroup()
+)
+
+season_finish = (
+    import_df
+    >> inner_join(_, counts, by = 'driver')
+    >> group_by(_.driver)
+    >> summarize(avg_finish = _.finish.mean().round(2))
+)
+
+last_5_finish = (
+    import_df
+    >> inner_join(_, counts, by = 'driver')
+    >> filter(_.race >= 16)
+    >> group_by(_.driver)
+    >> summarize(last_5_races = _.finish.mean().round(2))
+)
 
 # %%
-drivers = (
-    pl.read_csv('data/driver_data.csv')
-    .sort(pl.col('career_wins'), descending=True)
-    .head(n=5)
-    .with_columns(
-        pl.col('driver')
-        .cast(pl.Categorical, categories=drivers['driver'].unique())
+race = (
+    inner_join(season_finish, last_5_finish, by = 'driver')
+    >> arrange(_.avg_finish)
+    >> mutate(
+        change = case_when({
+            _.last_5_races < _.avg_finish: 'green',
+            _.last_5_races == _.avg_finish: 'gray',
+            _.last_5_races > _.avg_finish: 'red'
+        }),
+        # avg_finish = _.avg_finish * -1
     )
 )
 
-driver_colors = {
-    'Richard Petty': '#04aeec',
-    'David Pearson': '#630727',
-    'Jeff Gordon':  '#fc3812',
-    'Bobby Allison': '#e4be8f',
-    'Darrell Waltrip': '#24987a'
-}
-
 # %%
-xgap = 250
-
 (
-    ggplot(drivers)
+    race
+    >> ggplot()
     + geom_bar(
-        aes(x='reorder(driver, career_wins)', y='career_wins', fill='driver'), 
+        aes(x='reorder(driver, -avg_finish)', y='avg_finish * -1', fill='change'), 
+        stat='identity', color='black', alpha=0.7
+    )
+    + geom_bar(
+        aes(x='reorder(driver, -avg_finish)', y='last_5_races', fill='change'), 
         stat='identity', color='black', alpha=0.7
     )
     + geom_text(
-        drivers,
-        aes(x='driver', y='career_wins', label='career_wins'), 
-        nudge_y=-8, color='black'
+        aes(x='driver', y=-28, label='round(avg_finish, 1)'), 
+        nudge_y=-3, color='black', size=7
+    )
+    + annotate(
+        'text', x=37, y=-28, label='Season', size=7,
+        ha='right', va='top'
     )
     + geom_text(
-        aes(y=xgap, x='driver', label='round(career_win_pct, 4)*100'),
-        color='gray', size=8
+        aes(x='driver', y=28, label='round(last_5_races, 1)'), 
+        nudge_y=3, color='black', size=7
     )
-    + geom_hline(yintercept=235, color='gray')
+    + annotate(
+        'text', x=37, y=28, label='Last 5 Races', size=7,
+        ha='center', va='top'
+    )
     + coord_flip()
-    + theme_classic()
-    + scale_fill_manual(values=driver_colors)
     + labs(
-        title='NASCAR Cup Series Top-5 Winningest Drivers',
-        subtitle='Career wins and win percentage',
-        caption='Source: NASCAR.com (July 9, 2024)',
+        title='NASCAR Cup Series 2024 Driver\'s Average Finish',
+        subtitle='A comparison of season average finish versus average \nfinishing position over the last five races (through Chicago \nStreet Course on Sunday July 7, 2024)',
+        caption='Source: NASCAR.com\nRendered by: Kyle Grealis',
         x='', y=''
     )
-    +
+    + scale_fill_manual(values=['green', 'red'])
+    + theme_classic()
     + theme(
         legend_position='none',
-        plot_title=element_text(color='black', face='bold'),#, ha=-1.3),
+        plot_title=element_text(color='black', face='bold'),
         plot_subtitle=element_text(color='gray', face='bold'),
         plot_caption=element_text(color='gray'),
         axis_text=element_text(color='black'),
-        axis_text_x=element_blank()
+        axis_text_x=element_blank(),
     )
 )
